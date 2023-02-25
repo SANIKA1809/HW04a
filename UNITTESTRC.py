3be0dbf0c8e78fd01e4e5807270dc9eca58b4854
SANIKA1809 <101226043+SANIKA1809@users.noreply.github.com>
2023-02-25 01:13:38 -0500

    Update UNITTESTRC.py

--- UNITTESTRC.py
+++ UNITTESTRC.py
@@ -1,53 +1,17 @@
-import unittest
-from unittest.mock import MagicMock
+import requests
 import json
+import unittest
 
-# import the code to be tested
-import Repocommit
-
-class TestMyCode(unittest.TestCase):
-
+class TestGitHubAPI(unittest.TestCase):
+    
     def test_number_of_commits(self):
+        response = requests.get('https://api.github.com/users/SANIKA1809/repos')
+        data = json.loads(response.text)
 
-        # set up a mock response for the GitHub API request
-        mock_response = MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = [
-            {
-                "commits_url": "https://api.github.com/repos/SANIKA1809/repo1/commits{/sha}",
-                "name": "repo1"
-            },
-            {
-                "commits_url": "https://api.github.com/repos/SANIKA1809/repo2/commits{/sha}",
-                "name": "repo2"
-            }
-        ]
-
-        # set up a mock response for the commits API request
-        mock_commits_response = MagicMock()
-        mock_commits_response.status_code = 200
-        mock_commits_response.json.return_value = [
-            {
-                "sha": "commit1",
-                "commit": {
-                    "message": "commit message 1"
-                }
-            },
-            {
-                "sha": "commit2",
-                "commit": {
-                    "message": "commit message 2"
-                }
-            }
-        ]
-
-        # patch the requests module to return the mock responses
-        with unittest.mock.patch('requests.get') as mock_get:
-            mock_get.side_effect = [mock_response, mock_commits_response]
-
-            # call the function to be tested
-            result = Repocommit.get_number_of_commits('https://api.github.com/users/SANIKA1809/repos')
-
-            # assert the results
-            expected_result = {'repo1': 2, 'repo2': 2}
-            self.assertEqual(result, expected_result)
+        for repo in data:
+            commits_url = repo['commits_url'].replace('{/sha}', '')
+            commits_response = requests.get(commits_url)
+            commits_data = json.loads(commits_response.text)
+            num_commits = len(commits_data)
+            
+            self.assertTrue(num_commits >= 0)
