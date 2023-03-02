77727135c35764bf49d5dd2cc5cc7881e3d1357a
SANIKA1809 <101226043+SANIKA1809@users.noreply.github.com>
2023-03-01 19:13:13 -0500

    Update UNITTESTRC.py

--- UNITTESTRC.py
+++ UNITTESTRC.py
@@ -1,9 +1,13 @@
 import Repocommit
-
-def test_get_repo_commits():
-    username = "SANIKA1809"
-    repo_commits = Repocommit.get_repo_commits(username)
-    assert isinstance(repo_commits, dict)
-    assert len(repo_commits) > 0
-    assert all(isinstance(key, str) for key in repo_commits.keys())
-    assert all(isinstance(value, int) for value in repo_commits.values())
+import unittest
+class RepoCommitUsers(unittest.TestCase):
+    def test_get_repo_commits():
+        username = "SANIKA1809"
+        repo_commits = Repocommit.get_repo_commits(username)
+        assert isinstance(repo_commits, dict)
+        assert len(repo_commits) > 0
+        assert all(isinstance(key, str) for key in repo_commits.keys())
+        assert all(isinstance(value, int) for value in repo_commits.values())
+        
+if __name__ == '__main__':
+    unittest.main(exit=False)
